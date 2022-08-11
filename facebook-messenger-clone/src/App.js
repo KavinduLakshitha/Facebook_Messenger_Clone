b4bda702d9d7047a2c7c01c2fa4cd9968f6606f1
“your <it20154226@my.sliit.lk>
2022-08-11 08:05:55 +0530

    Base done!

--- facebook-messenger-clone/src/App.js
+++ facebook-messenger-clone/src/App.js
@@ -1,17 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import Message from "./Message";
 import { FormControl, Button, Input, InputLabel } from "@mui/material";
 
 function App() {
   const [input, setInput] = useState("");
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState([
+    { username: "kavindu", text: "hello" },
+    { username: "pasan", text: "yo whats up" },
+  ]);
+  const [username, setUsername] = useState("");
+
+  //useState = variable in ReactJS
+  // useEffect = a snippet of code that runs on a condition
+
+  useEffect(() => {
+    setUsername(prompt("Pleaser enter your name"));
+  }, []); // condtion
 
   console.log(input);
   console.log(messages);
 
   const sendMessage = (event) => {
     event.preventDefault();
-    setMessages([...messages, input]);
+    setMessages([...messages, { username: username, text: input }]);
     setInput("");
   };
 
@@ -19,25 +31,28 @@ function App() {
     <div className="App">
       <h1>Hello🚀</h1>
 
-      <FormControl>
-        <InputLabel>Enter a message...</InputLabel>
-        <Input
-          value={input}
-          onChange={(event) => setInput(event.target.value)}
-        />
-        <Button
-          variant="contained"
-          type="submit"
-          onClick={sendMessage}
-          disabled={!input}
-        >
-          Send Message
-        </Button>
-      </FormControl>
+      <form>
+        <FormControl>
+          <InputLabel>Enter a message...</InputLabel>
+          <Input
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+          <Button
+            variant="contained"
+            type="submit"
+            color="primary"
+            onClick={sendMessage}
+            disabled={!input}
+          >
+            Send Message
+          </Button>
+        </FormControl>
+      </form>
 
-      {messages.map((message) => {
-        <p>{message}</p>;
-      })}
+      {messages.map((message) => (
+        <Message username={username} message={message} />
+      ))}
     </div>
   );
 }
